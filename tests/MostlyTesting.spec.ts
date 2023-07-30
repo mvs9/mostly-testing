262ce26f595a59c041431f7223b87bde182db97e
mvsv0821 <mvsv0821@gmail.com>
2023-07-30 16:41:38 +0100

    Remove chromium launch

--- tests/MostlyTesting.spec.ts
+++ tests/MostlyTesting.spec.ts
@@ -3,7 +3,6 @@ import { test, expect, chromium } from '@playwright/test';
 let browser;
 
 test.beforeEach(async ({page}) => {
-  browser = await chromium.launch({ headless: false });
   await page.goto('https://mostly.ai/', { waitUntil: 'domcontentloaded' });
 })
 
